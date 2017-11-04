22f89c88b4d6a79411521a0f64968339ebbd5503
sevi221 <sevilaura221@gmail.com>
2017-11-04 14:10:11 +0100

    second commit

--- starter-code/lib/survey.js
+++ starter-code/lib/survey.js
@@ -1,23 +1,38 @@
+var responses = [
+  'This is the best job ever!',
+  'Satisfied.',
+  'At least I get paid.',
+  "I'm looking for another job.",
+  "I don't want to answer.",
+];
 
-var responses = [ "This is the best job ever!",
-                  "Satisfied.",
-                  "At least I get paid.",
-                  "I'm looking for another job.",
-                  "I don't want to answer." ];
-
-function randomSurvey(){
+function randomSurvey() {
   return Math.floor(Math.random() * responses.length);
 }
 console.log(randomSurvey());
 
-newArray = [];
 
-function tenRandom(){
 
-  for(i = 0; i < 10; i++){
+function tenRandom() {
+  newArray = [];
+  for (i = 0; i < 10; i++) {
     newArray.push(responses[randomSurvey()]);
   }
-
+  return newArray;
 }
 
-tenRandom();
+//var surveyDepartments = [tenRandom(), tenRandom(), tenRandom(), tenRandom(), tenRandom()];
+//console.log(surveyDepartments);
+
+
+function survey(){
+  surveyDepartments = [];
+  for (i = 0; i < 5; i++) {
+    console.log(i);
+    surveyDepartments[i] = tenRandom();
+    //console.log(surveyDepartments[i]);
+  }
+  return surveyDepartments;
+}
+
+console.log(survey());
